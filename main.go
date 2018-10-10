af615f8932d6c5b113aa8924873856a463570fe5
JorgenJJ <jorgen.jarnes@gmail.com>
2018-10-10 15:27:05 +0200

    init

--- main.go
+++ main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-	"io"
-	"net/http"
-	"os"
+	"fmt"
+	"github.com/marni/goigc"
 )
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello world!")
-}
-
 func main() {
-	port := os.Getenv("PORT")
-	http.HandleFunc("/", hello)
-	http.ListenAndServe(":"+port, nil)
+	s := "http://skypolaris.org/wp-content/uploads/IGS%20Files/Madrid&20to%20Jerez.igc"
+	track, err := igc.ParseLocation(s)
+	if err != nil {
+		fmt.Errorf("Problem reading the track", err)
+	}
+
+	fmt.Printf("Pilot: %s, gilderType: %s, date: %s",
+		track.Pilot, track.GliderType, track.Date.String())
 }
