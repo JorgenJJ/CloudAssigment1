9272457c9ef2eb39f3d7dd57efaca2044b252de8
JorgenJJ <jorgen.jarnes@gmail.com>
2018-10-10 16:54:58 +0200

    New version

--- main.go
+++ main.go
@@ -3,15 +3,26 @@ package main
 import (
 	"fmt"
 	"github.com/marni/goigc"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
-	s := "http://skypolaris.org/wp-content/uploads/IGS%20Files/Madrid%20to%20Jerez.igc"
+	/*s := "http://skypolaris.org/wp-content/uploads/IGS%20Files/Madrid%20to%20Jerez.igc"
 	track, err := igc.ParseLocation(s)
 	if err != nil {
 		fmt.Errorf("Problem reading the track", err)
 	}
 
 	fmt.Printf("Pilot: %s, gilderType: %s, date: %s",
-		track.Pilot, track.GliderType, track.Date.String())
+		track.Pilot, track.GliderType, track.Date.String())*/
+
+	response, err := http.Get(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	else {
+		fmt.Print(response)
+	}
 }
