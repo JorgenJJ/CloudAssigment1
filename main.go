71d834b2ab40a1a533163dc80800ec85de8e2ce1
JorgenJJ <jorgen.jarnes@gmail.com>
2018-10-11 14:09:15 +0200

    :D

--- main.go
+++ main.go
@@ -7,16 +7,15 @@ import (
 )
 
 /*func main() {
-	/*s := "http://skypolaris.org/wp-content/uploads/IGS%20Files/Madrid%20to%20Jerez.igc"
+	s := "http://skypolaris.org/wp-content/uploads/IGS%20Files/Madrid%20to%20Jerez.igc"
 	track, err := igc.ParseLocation(s)
 	if err != nil {
 		fmt.Errorf("Problem reading the track", err)
 	}
 
 	fmt.Printf("Pilot: %s, gilderType: %s, date: %s",
-		track.Pilot, track.GliderType, track.Date.String())*/
+		track.Pilot, track.GliderType, track.Date.String())
 
-	/*
 	if len(os.Args) != 2 {
 		os.Exit(1)
 	}
@@ -59,3 +58,4 @@ func main() {
 
 	http.HandleFunc("/", pageNotFound)
 }
+*/
